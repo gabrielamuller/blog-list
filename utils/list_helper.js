930ffb76ef054008ee69082fbccd06943550ce1e
gabrielamuller <mullergabrielaa@gmail.com>
2022-11-19 17:39:46 +0200

    4.3 Add a new test for total likes of one blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
   return 1
 }
+
+const totalLikes = (blogs) => {
+  if (blogs.length > 0) {
+    return blogs.reduce((sum, likes) => sum + likes)
+  } else {
+    return 0
+  }
+}
   
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
